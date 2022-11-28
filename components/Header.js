8e88ce0170586b90f8df453de777d939a00b65e5
Bartek <s210869@sggw.edu.pl>
2022-11-28 19:39:29 +0100

    added styling to header component

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,20 @@
 import { ConnectButton } from "@web3uikit/web3";
 import Link from "next/link";
+import styles from "../styles/header.module.css";
 
 export default function Header() {
     return (
-        <nav>
-            <Link href="/">Vote Page</Link>
-            <Link href="/admin">Admin Page</Link>
-            <ConnectButton></ConnectButton>
+        <nav className={styles.navbar}>
+            <h1 className={styles.title}>Election DApp</h1>
+            <div className={styles.navbarButtonsContainer}>
+                <Link className={styles.navbarButton} href="/">
+                    Vote Page
+                </Link>
+                <Link className={styles.navbarButton} href="/admin">
+                    Admin Page
+                </Link>
+                <ConnectButton moralisAuth={false}></ConnectButton>
+            </div>
         </nav>
     );
 }
